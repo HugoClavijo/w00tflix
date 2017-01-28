50bde42ba1b92fd462547548ad3102cbc4e17ddd
Hugo Clavijo <hugoclavijo@outlook.com>
2017-01-28 21:42:05 +0000

    livesearh

--- livesearch.php
+++ livesearch.php
@@ -35,7 +35,7 @@ if (strlen($q)>0) {
 // Set output to "no suggestion" if no hint was found
 // or to the correct values
 if ($hint=="") {
-  $response="no hay sugerencias";
+  $response="no hay sugerencias...";
 } else {
   $response=$hint;
 }
