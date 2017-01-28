d3941aa4284f81bf7f52dab9d55944eb7952e0a1
Hugo Clavijo <hugoclavijo@outlook.com>
2017-01-28 21:40:08 +0000

    livesearh

--- index2.php
+++ index2.php
@@ -366,6 +366,6 @@
          <script src="assets/js/skel.min.js"></script>
          <script src="assets/js/util.js"></script>
          <script src="assets/js/main.js"></script>
-         <script type="text/javascript" src="assets/js/livesearch.js"></script><!--Pilas.................. -->
+         <script type="text/javascript" src="assets/js/livesearch.js"></script>
    </body>
 </html>
