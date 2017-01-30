06853ccb4e81da5071fb5ff903a39a1cf0bb199c
David Robelly <david.robelly@gmail.com>
2017-01-29 23:55:53 -0500

    script de base de datos con 3 tablas agregadas

--- scriptDB_w00tflix.sql.sql
+++ scriptDB_w00tflix.sql.sql
@@ -7,3 +7,43 @@ CREATE DATABASE w00tflix
        LC_CTYPE = 'es_EC.UTF-8'
        CONNECTION LIMIT = -1;
 
+CREATE TABLE public.categorias
+(
+  idcategorias integer NOT NULL,
+  nombre character varying(20),
+  descipcion character varying(30),
+  CONSTRAINT "PK" PRIMARY KEY (idcategorias),
+  CONSTRAINT unico UNIQUE (idcategorias)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.categorias
+  OWNER TO postgres;
+
+
+CREATE TABLE public.comentarios
+(
+  idcomentarios integer NOT NULL,
+  idusuario integer,
+  idvideo integer,
+  comentario character varying(140),
+  CONSTRAINT "PKCOMENTARIOS" PRIMARY KEY (idcomentarios)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.comentarios
+  OWNER TO postgres;
+
+CREATE TABLE public.detalle_videos
+(
+  iddetalles integer NOT NULL,
+  descripcion character varying(30),
+  CONSTRAINT "PKDETALLES" PRIMARY KEY (iddetalles)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.detalle_videos
+  OWNER TO postgres;
