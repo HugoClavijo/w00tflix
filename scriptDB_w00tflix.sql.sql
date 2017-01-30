e9bdba028f88a7d7ed822fb162cc9a8e0a1a8855
David Robelly <david.robelly@gmail.com>
2017-01-29 23:54:07 -0500

    script de la abse de datos

---
+++ scriptDB_w00tflix.sql.sql
@@ -0,0 +1,9 @@
+
+CREATE DATABASE w00tflix
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'es_EC.UTF-8'
+       LC_CTYPE = 'es_EC.UTF-8'
+       CONNECTION LIMIT = -1;
+
