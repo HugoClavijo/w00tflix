3dc230d052b9f7ab1aeb0616c625f86708508d3a
unknown <carrera1995@hotmail.es>
2017-02-20 21:27:33 +0000

    update

--- admin/usuarios/update.php
+++ admin/usuarios/update.php
@@ -48,3 +48,195 @@ echo $idusuarios." ".$nombre." ".$tipo." ";
 	
 	<input id="submit_button" type="submit" value="Enviar" />
 </form>
+
+
+
+
+
+<!DOCTYPE html>
+ 
+<html lang="es">
+ 
+<head>
+<title>Titulo de la web</title>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="../../estilos.css" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="alternate" title="Pozolería RSS" type="application/rss+xml" href="/feed.rss" />
+
+
+   <!-- jQuery UI -->
+    <link href="https://code.jquery.com/ui/1.10.3/themes/redmond/jquery-ui.css" rel="stylesheet" media="screen">
+
+    <!-- Bootstrap -->
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- styles -->
+    <link href="../../assets/css/styles.css" rel="stylesheet">
+
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link href="../../assets/vendors/form-helpers/css/bootstrap-formhelpers.min.css" rel="stylesheet">
+    <link href="../../assets/vendors/select/bootstrap-select.min.css" rel="stylesheet">
+    <link href="../../assets/vendors/tags/css/bootstrap-tags.css" rel="stylesheet">
+
+    <link href="../../assets/css/forms.css" rel="stylesheet">
+</head>
+ 
+<body>
+
+<div class="header">
+	     <div class="container">
+	        <div class="row">
+	           <div class="col-md-12">
+	              <!-- Logo -->
+	              <div class="logo">
+	                 <h1> <a href="#">Usuarios</a></h1>
+	              </div>
+	           </div>
+	        </div>
+	     </div>
+	</div>
+
+
+<div class="row">
+<div class="content-box-large">
+<div class="panel-heading">
+<div class="panel-body">
+ <form class="form-horizontal" role="form">
+
+
+
+
+								  <div class="form-group">
+								    <label class="col-sm-2 control-label">Id usuario</label>
+								    <div class="col-sm-10 col-md-2">
+								      <span class="form-control">Aqui va el número</span>
+								    </div>
+								  </div>
+
+
+
+
+								  <div class="form-group">
+								    <label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
+								    <div class="col-sm-10">
+								      <input type="email" class="form-control" id="inputEmail3" placeholder="Ej. Jose Eduardo Perez">
+								    </div>
+								  </div>
+
+
+								  <div class="form-group">
+								    <label for="inputEmail3" class="col-sm-2 control-label">Correo</label>
+								    <div class="col-sm-10">
+								      <input type="email" class="form-control" id="inputEmail3" placeholder="tucorreo@hotmail.com">
+								    </div>
+								  </div>
+
+								  <div class="form-group">
+								    <label for="inputEmail3" class="col-sm-2 control-label">Tipo Usuario</label>
+								    <div class="col-sm-10">
+								      <input type="email" class="form-control" id="inputEmail3" placeholder="admin(1), usuario (2)">
+								    </div>
+								  </div>
+
+								  <div class="form-group">
+								    <label for="inputEmail3" class="col-sm-2 control-label">Usuario</label>
+								    <div class="col-sm-10">
+								      <input type="email" class="form-control" id="inputEmail3" placeholder="Escribe un nombre corto">
+								    </div>
+								  </div>
+
+
+
+
+
+
+								  <div class="form-group">
+								    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+								    <div class="col-sm-10">
+								      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+								    </div>
+								  </div>
+
+
+
+
+
+
+
+
+								  <!--div class="form-group">
+								    <label class="col-sm-2 control-label">Textarea</label>
+								    <div class="col-sm-10">
+								      <textarea class="form-control" placeholder="Textarea" rows="3"></textarea>
+								    </div>
+								  </div-->
+
+
+
+
+								  <!--div class="form-group">
+								    <div class="col-sm-offset-2 col-sm-10">
+								      <div class="checkbox">
+								        <label>
+								          <input type="checkbox"> Checkbox
+								        </label>
+								      </div>
+								      <div class="checkbox">
+								        <label>
+								          <input type="checkbox"> Checkbox
+								        </label>
+								      </div>
+								    </div>
+								  </div-->
+
+
+								  <div class="form-group">
+								    <div class="col-sm-offset-2 col-sm-10">
+								      <button type="submit" class="btn btn-primary">Sign in</button>
+								    </div>
+								  </div>
+								</form>
+
+
+</div>
+</div>
+</div>
+</div>
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- jQuery UI -->
+    <script src="https://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../../bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="../../assets/vendors/form-helpers/js/bootstrap-formhelpers.min.js"></script>
+
+    <script src="../../assets/vendors/select/bootstrap-select.min.js"></script>
+
+    <script src="../../assets/vendors/tags/js/bootstrap-tags.min.js"></script>
+
+    <script src="../../assets/vendors/mask/jquery.maskedinput.min.js"></script>
+
+    <script src="../../assets/vendors/moment/moment.min.js"></script>
+
+    <script src="../../assets/vendors/wizard/jquery.bootstrap.wizard.min.js"></script>
+
+     <!-- bootstrap-datetimepicker -->
+     <link href="../../assets/vendors/bootstrap-datetimepicker/datetimepicker.css" rel="stylesheet">
+     <script src="../../assets/vendors/bootstrap-datetimepicker/bootstrap-datetimepicker.js"></script> 
+
+
+    <link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet"/>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
+
+    <script src="../../assets/js/custom.js"></script>
+    <script src="../../assets/js/forms.js"></script>
+
+
+</body>
+</html>
+
+
